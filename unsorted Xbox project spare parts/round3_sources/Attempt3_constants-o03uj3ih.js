aa318dd655575aa77d8f258db2ce64c053e4dc44
David williams <117888838+1456319@users.noreply.github.com>
2026-03-28 22:47:10 -0400

    Push batch 3 of files from round3_sources to GitHub.

---
+++ unsorted Xbox project spare parts/round3_sources/Attempt3_constants-o03uj3ih.js
@@ -0,0 +1,1 @@
+try{let e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`o03uj3ih-0ff4-4f65-99ce-5e4188626bab`,e._sentryDebugIdIdentifier=`sentry-dbid-o03uj3ih-0ff4-4f65-99ce-5e4188626bab`)}catch{}var e=`constants`;export{e as t};
---
+++ unsorted Xbox project spare parts/round3_sources/Attempt3_hooks-glsib73c.js
@@ -0,0 +1,1 @@
+try{let e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`glsib73c-0ff4-4f65-99ce-5e4188626bab`,e._sentryDebugIdIdentifier=`sentry-dbid-glsib73c-0ff4-4f65-99ce-5e4188626bab`)}catch{}var e=e=>e;export{e as hooks};
