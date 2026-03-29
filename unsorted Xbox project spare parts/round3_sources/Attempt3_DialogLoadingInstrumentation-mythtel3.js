f729fba9f942452516910a88d50f52178a0d7c27
David williams <117888838+1456319@users.noreply.github.com>
2026-03-28 22:48:30 -0400

    Push batch 5 of JS files from round3_sources to GitHub.

---
+++ unsorted Xbox project spare parts/round3_sources/Attempt3_DialogLoadingInstrumentation-mythtel3.js
@@ -0,0 +1,1 @@
+try{let e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`mythtel3-0ff4-4f65-99ce-5e4188626bab`,e._sentryDebugIdIdentifier=`sentry-dbid-mythtel3-0ff4-4f65-99ce-5e4188626bab`)}catch{}var e=e=>e;export{e as DialogLoadingInstrumentation};
